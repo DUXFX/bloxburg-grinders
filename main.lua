7a48f44853c7033d62d651791656f27d714b8542
iopsec <xi.opsec@gmail.com>
2025-03-29 21:09:08 +1300

    Added Pizza Delivery Farm.

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ if failed_count > 0 then
     return;
 end
 
-local debug_enabled = false;
+local debug_enabled = true;
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/iopsec/bloxburg-grinders/main/ui.lua"))();
 
 -- utils
@@ -78,11 +78,14 @@ end
 local player = utils:find_from("Players.LocalPlayer");
 local modules = utils:wait_for("PlayerScripts.Modules", player);
 local data_service = utils:wait_for("ReplicatedStorage.Modules.DataService");
-local job_module = require(utils:wait_for("JobHandler", modules));
+local job_module_path = utils:wait_for("JobHandler", modules);
+local job_module = require(job_module_path);
 local interaction_module = require(utils:wait_for("InteractionHandler", modules));
 local locations = utils:wait_for("Workspace.Environment.Locations");
 local pathfinding_service = game:GetService("PathfindingService");
-local virtual_user_service = game:GetService("VirtualUser")
+local tween_service = game:GetService("TweenService");
+local virtual_user_service = game:GetService("VirtualUser");
+local run_service = game:GetService("RunService");
 
 -- anti afk
 player.Idled:Connect(function()
@@ -91,10 +94,34 @@ player.Idled:Connect(function()
     virtual_user_service:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame);
 end)
 
+-- job functions
+local job_utils = {} do
+    function job_utils:is_working()
+        setthreadidentity(2);
+        local current_job = job_module:GetJob();
+        setthreadidentity(our_identity);
+        return current_job ~= nil, current_job;
+    end
+
+    function job_utils:start_shift(job)
+        local is_working, current_job = self:is_working();
+        if is_working then
+            return false, "Already working.";
+        end
+        setthreadidentity(2);
+        job_module:GoToWork(job);
+        setthreadidentity(our_identity);
+    end
+
+    function job_utils:end_shift()
+        local end_shift_btn = utils:wait_for("PlayerGui.MainGUI.Bar.CharMenu.WorkFrame.WorkFrame.Action", player);
+        firesignal(end_shift_btn.Activated);
+    end
+end
+
 -- pathfinding (copied from roblox <3)
 local pathfinding = {} do
     function pathfinding:walk_to(target, no_jump)
-        print("Finding path to", target)
         local path = pathfinding_service:CreatePath();
         local waypoints, next_waypoint_idx, reached_connection, blocked_connection;
         local completed = false;
@@ -107,7 +134,7 @@ local pathfinding = {} do
         local humanoid = character:WaitForChild("Humanoid");
         local success, err_message = pcall(function()
             path:ComputeAsync(character.PrimaryPart.Position, _type == "Vector3" and target or target.Position);
-        end)
+        end);
 
         if success and path.Status == Enum.PathStatus.Success then
             waypoints = path:GetWaypoints();
@@ -156,11 +183,6 @@ end
 local hairdressers = {
     do_actions = {}
 } do
-    function hairdressers:start_shift()
-        setthreadidentity(2);
-        job_module:GoToWork("StylezHairdresser");
-        setthreadidentity(our_identity);
-    end
 
     function hairdressers:get_do_actions()
         if #hairdressers.do_actions == 4 then
@@ -323,9 +345,12 @@ local hairdressers = {
 
     function hairdressers:toggle_farming(state)
         if state then
-            if not job_module.IsWorking() then
-                hairdressers:start_shift();
-                task.wait(1);
+            local is_working, current_job = job_utils:is_working();
+            if (not is_working) or (is_working and current_job ~= "StylezHairdresser") then
+                if is_working then
+                    job_utils:end_shift();
+                end
+                job_utils:start_shift("StylezHairdresser");
             end
     
             hairdressers:get_workstation();
@@ -369,18 +394,20 @@ local ice_cream = { farming = false, integrity = 0, connections = {}, orders_com
 
         if not self.farming then
             self.integrity += 1
-            disable_interaction = false;
             self.orders_completed = 0;
             return
         end
 
-        disable_interaction = true;
+        local is_working, current_job = job_utils:is_working();
+        if (not is_working) or (is_working and current_job ~= "BensIceCreamSeller") then
+            if is_working then
+                job_utils:end_shift();
+            end
 
-        if job_module:GetJob() ~= "BensIceCreamSeller" then
-            setthreadidentity(2);
-            job_module:GoToWork("BensIceCreamSeller");
-            setthreadidentity(our_identity);
+            job_utils:start_shift("BensIceCreamSeller");
+
             task.wait(1);
+
             player.Character.Humanoid:MoveTo(positions.front_counter);
             repeat task.wait() until 5 >= player:DistanceFromCharacter(positions.front_counter);
         end
@@ -625,19 +652,18 @@ local supermarket_cashier = { farming = false,  orders_completed = 0 }; do
         end
 
         if not self.farming then
-            disable_interaction = false;
             self.orders_completed = 0;
             return
         end
 
-        disable_interaction = true;
-
-        if job_module:GetJob() ~= "SupermarketCashier" then
-            setthreadidentity(2);
-            job_module:GoToWork("SupermarketCashier");
-            setthreadidentity(our_identity);
-            self:claim_workstation();
+        local is_working, current_job = job_utils:is_working();
+        if is_working then
+            if current_job ~= "SupermarketCashier" then
+                job_utils:end_shift();
+            end
         end
+
+        job_module:start_shift("SupermarketCashier");
         
         coroutine.wrap(function()
             local current_order;
@@ -660,11 +686,182 @@ local supermarket_cashier = { farming = false,  orders_completed = 0 }; do
     end
 end
 
+local pizza_delivery = { current_customer = nil, max_speed = 50 }; do
+    for _, v in next, workspace.Environment["Roads [LOCAL]"]:GetDescendants() do
+        if v.Name == "Road" or v.Name == "Pavement" then
+            local modifier = Instance.new("PathfindingModifier");
+            modifier.Label = v.Name;
+            modifier.Parent = v;
+        end
+    end
+
+    function pizza_delivery:get_job()
+        local is_working, current_job = job_utils:is_working();
+        if is_working then
+            if current_job ~= "PizzaPlanetDelivery" then
+                job_utils:end_shift();
+            else
+                return;
+            end
+        end
+
+        self.status.Text = "Status: Getting Job."
+
+        job_utils:start_shift("PizzaPlanetDelivery");
+    end
+
+    function pizza_delivery:our_moped()
+        return player.Character:FindFirstChild("Vehicle_Delivery Moped");
+    end
+
+    function pizza_delivery:claim_moped()
+        local moped_model = utils:find("PizzaPlanet.DeliveryMoped", locations);
+        if not moped_model then
+            return nil;
+        end
+
+        if 6 < player:DistanceFromCharacter(moped_model.PrimaryPart.Position) then
+            pathfinding:walk_to(moped_model.PrimaryPart.Position);
+        end
+
+        repeat
+            interaction:quick_interact(moped_model, "Use");
+            task.wait(0.5);
+        until self:our_moped();
+
+        return self:our_moped();
+    end
+
+    function pizza_delivery:grab_box()
+        local has_box = utils:find("Pizza Box", player.Character);
+        if has_box then
+            return has_box;
+        end
+
+        local boxes = utils:wait_for("PizzaPlanet.Conveyor.MovingBoxes", locations);
+        for _, v in next, boxes:GetChildren() do
+            interaction:quick_interact(v, "Take", v);
+            task.wait(0.25);
+            if utils:find("Pizza Box", player.Character) then
+                break;
+            end
+        end
+
+        return utils:find("Pizza Box", player.Character);
+    end
+
+    function pizza_delivery:complete_order()
+        local moped = self:our_moped();
+        if not moped then
+            self.status.Text = "Status: Getting moped.";
+            moped = self:claim_moped();
+        end
+        
+        self.status.Text = "Status: Getting pizza.";
+        moped:PivotTo(CFrame.new(1169, 15, 273))
+        
+        repeat
+            self:grab_box();
+            task.wait(0.3);
+        until self.current_customer ~= nil;
+        
+        self.status.Text = "Status: Going to customer.";
+        local time_start = tick();
+        moped:PivotTo(CFrame.new(1169, -45, 273))
+        
+        local customer_cframe = self.current_customer:WaitForChild("HumanoidRootPart").CFrame;
+        
+        self:move_to(customer_cframe - Vector3.new(0, 45, 0));
+        
+        player.Character.PrimaryPart.Anchored = true;
+        repeat 
+            self.status.Text = `Status: Waiting for {18 - math.floor(tick() - time_start)} seconds to pass.`;
+            task.wait();
+        until (tick() - time_start) > 18;
+
+        self.status.Text = "Status: Giving pizza.";
+        player.Character.PrimaryPart.Anchored = false;
+        
+        repeat 
+            moped:PivotTo(customer_cframe - Vector3.new(0, 5, 0))
+            interaction:quick_interact(self.current_customer, "Give");
+            task.wait(0.2);
+        until not player.Character:FindFirstChild("Pizza Box");
+        
+        self.status.Text = "Status: Complete! Going to pizza place.";
+        moped:PivotTo(customer_cframe - Vector3.new(0, 45, 0));
+        self:move_to(Vector3.new(1169, -45, 273));
+
+        player.Character.PrimaryPart.Anchored = true;
+        repeat 
+            self.status.Text = `Status: Waiting for {36 - math.floor(tick() - time_start)} seconds to pass.`;
+            task.wait();
+        until (tick() - time_start) > 36;
+        player.Character.PrimaryPart.Anchored = false;
+
+        moped:PivotTo(CFrame.new(1169, 15, 273));
+    end
+
+    function pizza_delivery:toggle_farming(state)
+        self.farming = state;
+        if self.farming then
+            self:get_job();
+            task.wait(1);
+            coroutine.wrap(function()
+                while self.farming do
+                    self:complete_order();
+                    task.wait();
+                end
+            end)();
+        else
+            self.status.Text = "Status: Disabled.";
+        end
+    end
+
+    function pizza_delivery:move_to(position)
+        local moped = self:our_moped();
+        if moped then
+            if moped.PrimaryPart:FindFirstChildOfClass("BodyVelocity") then
+                moped.PrimaryPart:FindFirstChildOfClass("BodyVelocity"):Destroy();
+            end
+            
+            local under_map_cframe = CFrame.new(position.X, -45, position.Z)
+            local body_velocity = Instance.new("BodyVelocity");
+            body_velocity.P = 30;
+            body_velocity.MaxForce = Vector3.new(9e9, 9e9, 9e9);
+            body_velocity.Parent = moped.PrimaryPart;
+
+            repeat
+                body_velocity.Velocity = (under_map_cframe.Position - moped.PrimaryPart.Position).Unit * 30;
+                task.wait();
+            until (moped.PrimaryPart.Position - under_map_cframe.Position).Magnitude < 5;
+
+            body_velocity.Velocity = Vector3.new(0, 0, 0);
+        else
+            self:claim_moped();
+            pizza_delivery:move_to(position)
+        end
+    end
+end
+
+-- hooking
+local old_mt; old_mt = hookmetamethod(game, "__namecall", function(...)
+    local args = {...};
+    if getnamecallmethod() == "InvokeServer" and string.match(debug.traceback(), "JobHandler.PizzaPlanetDelivery") then
+        if args[2] and rawget(args[2], "Box") then
+            pizza_delivery.current_customer = old_mt(...);
+            return pizza_delivery.current_customer;
+        end
+    end
+    return old_mt(...);
+end);
+
 library:create_window("Bloxburg Grinders", 250);
 
 local hair_tab = library:add_section("Hairdressers");
 local ice_cream_tab = library:add_section("Ben's Ice Cream");
 local supermarket_cashier_tab = library:add_section("Supermarket Cashier");
+local pizza_delivery_tab = library:add_section("Pizza Planet Delivery");
 
 hair_tab:add_toggle("Autofarm", "hair_farm", function(state)
     hairdressers:toggle_farming(state);
@@ -683,3 +880,9 @@ supermarket_cashier_tab:add_toggle("Autofarm", "market_cashier_farm", function(s
 end);
 
 supermarket_cashier_tab:add_toggle("Legit Mode", "market_cashier_farm_legit", function() end);
+
+pizza_delivery_tab:add_toggle("Autofarm", "pizza_delivery_farm", function(state)
+    pizza_delivery:toggle_farming(state);
+end);
+
+pizza_delivery.status = pizza_delivery_tab:add_label("Status: Disabled.").Text;
